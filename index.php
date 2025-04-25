6464ff5fe4ee9b79187c9845881f75f8adf2d22b
Verathagnus <59093644+Verathagnus@users.noreply.github.com>
2025-04-25 20:55:32 +0530

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+function isMobileDevice() {
+    // Check the user agent string to detect a mobile device
+    $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
+    $mobileAgents = ['iphone', 'android', 'ipad', 'ipod', 'blackberry', 'opera mini', 'iemobile', 'mobile'];
+
+    // If the user agent matches one of the mobile devices
+    foreach ($mobileAgents as $agent) {
+        if (strpos($userAgent, $agent) !== false) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Redirect to the appropriate index file
+if (isMobileDevice()) {
+    header('Location: index_mobile.html'); // Redirect to the mobile version
+} else {
+    header('Location: index_desktop.html'); // Redirect to the desktop version
+}
+exit();
+?>
